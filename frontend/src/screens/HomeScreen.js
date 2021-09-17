a91255cb0820586a35bca1736474c7c127ecbcb5
Chad Tarpey <chadtarpey@gmail.com>
2021-09-17 16:03:25 -0500

    created Loader and Message js files

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import Product from "../components/Product"
 import { useDispatch, useSelector } from "react-redux"
 import { Row, Col } from "react-bootstrap"
 import { listProducts } from "../actions/productAction.js"
+import Message from "../components/Message.js"
+import Loader from "../components/Loader.js"
 
 const HomeScreen = () => {
 
@@ -20,7 +22,7 @@ const HomeScreen = () => {
     return (
         <>
             <h1>Latest Products</h1>
-            {loading ? <h2>Loading.. </h2> : error ? <h3>{error}</h3> :
+            {loading ? <Loader /> : error ? <Message variant="danger">{error}</Message> :
                 <Row>
                     {products.map(product => (
                         <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
