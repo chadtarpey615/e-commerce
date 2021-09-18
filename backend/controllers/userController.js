96b781fa8fc701e126d718d62ab32e16acc6c60a
Chad Tarpey <chadtarpey@gmail.com>
2021-09-18 17:03:38 -0500

    created userController and can log user with auth in backend testing through postman

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,5 +24,28 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc GET user user profile
+// @route  POST /api/users/profile
+// @access private
+const getUserProfile = asyncHandler(async (req, res) => {
 
-export { authUser }
+
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error("User not found")
+    }
+
+})
+
+
+
+export { authUser, getUserProfile }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express from "express"
-import { authUser } from "../controllers/userController.js"
+import { authUser, getUserProfile } from "../controllers/userController.js"
+import { protect } from "../middleware/authMiddleware.js"
 const router = express.Router()
 
 
@@ -7,6 +8,7 @@ const router = express.Router()
 // @route  GET /api/products
 // @access public
 router.post("/login", authUser)
+router.route("/profile").get(protect, getUserProfile)
 
 
 
