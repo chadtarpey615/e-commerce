940e9e643e9f50ec374115bab7fab464066f78bc
Chad Tarpey <chadtarpey@gmail.com>
2021-09-19 11:28:34 -0500

    created a reducer and action for user login

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { authUser, getUserProfile } from "../controllers/userController.js"
+import { authUser, getUserProfile, registerUser } from "../controllers/userController.js"
 import { protect } from "../middleware/authMiddleware.js"
 const router = express.Router()
 
@@ -7,6 +7,7 @@ const router = express.Router()
 // @desc fetch all products 
 // @route  GET /api/products
 // @access public
+router.route("/").post(registerUser)
 router.post("/login", authUser)
 router.route("/profile").get(protect, getUserProfile)
 
