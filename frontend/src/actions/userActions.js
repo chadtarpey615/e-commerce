58f202b70b190bd6bd4fc7a2eb39f2a04505edfc
Chad Tarpey <chadtarpey@gmail.com>
2021-09-20 14:05:09 -0500

    created RegisterScreen and can register new users on the client side

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,6 +1,18 @@
-import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL } from "../constants/userConstants"
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL } from "../constants/userConstants"
 import axios from "axios"
+
+
 export const login = (email, password) => async dispatch => {
+
+}
+
+export const logout = () => (dispatch) => {
+    localStorage.removeItem("userInfo")
+    dispatch({ type: USER_LOGOUT })
+}
+
+export const register = (name, email, password) => async (dispatch) => {
+
     try {
         dispatch({
             type: USER_LOGIN_REQUEST,
@@ -12,13 +24,18 @@ export const login = (email, password) => async dispatch => {
             }
         }
 
-        const { data } = await axios.post("/api/users/login",
+        const { data } = await axios.post("/api/users",
             {
-                email, password
+                name, email, password
             },
             config
         )
 
+
+        dispatch({
+            type: USER_REGISTER_SUCCESS,
+            payload: data
+        })
 
         dispatch({
             type: USER_LOGIN_SUCCESS,
@@ -28,8 +45,10 @@ export const login = (email, password) => async dispatch => {
         localStorage.setItem("userInfo", JSON.stringify(data))
     } catch (error) {
         dispatch({
-            type: USER_LOGIN_FAIL,
+            type: USER_REGISTER_FAIL,
             payload: error.response && error.response.data.message ? error.response.data.message : error.message,
         })
     }
+
+
 }
