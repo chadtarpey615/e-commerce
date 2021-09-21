e249eec86b78f273474cad20bf67eff3c4224e24
Chad Tarpey <chadtarpey@gmail.com>
2021-09-21 13:12:06 -0500

    created a order controller and route in the back order to be able place orders

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,31 @@
+import Order from "../models/orderModel.js"
+import asyncHandler from "express-async-handler"
+
+
+// @desc Create new order 
+// @route  POST /api/orders
+// @access private
+const addOrderItems = asyncHandler(async (req, res) => {
+    const { orderItems, shippingAddress, paymentMethod, itemsPrice, taxPrice, shippingPrice, totalPrice } = req.body
+
+    if (orderItems && orderItems.length === 0) {
+        res.status(400)
+        throw new Error("no reder items")
+    } else {
+        const order = new Order({
+            orderItems,
+            user: req.user._id,
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
+            totalPrice
+        })
+
+        const createdOrder = await order.save()
+        res.status(201).json(createdOrder)
+    }
+})
+
+export { addOrderItems }
